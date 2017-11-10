e624e8f8420f14d9ba23a172b267e9d5dbd7f6da
julieKlo <32794236+julieKlo@users.noreply.github.com>
2017-11-10 16:15:09 +0100

    Delete CTestTestfile.cmake

--- 12500_neurones/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/localguest/Desktop/myfiles/BA3/premier_projet/Neuron_project_2017/12500_neurones
-# Build directory: /home/localguest/Desktop/myfiles/BA3/premier_projet/Neuron_project_2017/12500_neurones
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Neurons_unittest1 "Neurons_unittest1")
-subdirs(gtest)
